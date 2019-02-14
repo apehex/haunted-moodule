0089eec257995211c7b54a25d2fa7ea3a67e4bcf
David Mougeolle <david.mougeolle@moodule.net>
2019-02-14 22:17:38 +0100

    Keep only the files I actually work on

--- docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Haunted Moodule in a project::
-
-    import haunted-moodule
--- haunted-moodule/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Haunted Moodule."""
-
-__author__ = """David Mougeolle"""
-__email__ = 'david.mougeolle@moodule.net'
-__version__ = '0.0.0'
